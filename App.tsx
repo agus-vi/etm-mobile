efdc4193fe0eba23f2bf2b421d01d3b0a1bc7693
agus-vi <agux_vi@hotmail.com>
2025-04-26 15:14:03 -0300

    cambio a expo

--- App.tsx
+++ App.tsx
@@ -1,58 +1,20 @@
-import React from "react";
-import type { PropsWithChildren } from "react";
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from "react-native";
-import {
-  Button,
-  SegmentedControl,
-  Text,
-  TextField,
-  View,
-} from "react-native-ui-lib";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const safePadding = "5%";
-
+export default function App() {
   return (
-    <View>
-      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-      <ScrollView>
-        <View flex center>
-          <Text blue50 text20 marginB-s5>
-            Welcome
-          </Text>
-          <SegmentedControl
-            segments={[{ label: "Register" }, { label: "Login" }]}
-          />
-
-          <View marginT-s5>
-            <TextField preset="outline" placeholder="username" />
-            <TextField
-              preset="outline"
-              placeholder="password"
-              secureTextEntry
-              grey10
-            />
-          </View>
-
-          <View row marginT-s5 centerV>
-            <Button link text70 orange30 label="Sign Up" marginR-s5 />
-            <Button text70 white background-orange30 label="Login" />
-          </View>
-        </View>
-      </ScrollView>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
