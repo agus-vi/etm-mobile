208da4a87e44ab14a3dbc28ad5b8f50e77223618
agus-vi <agux_vi@hotmail.com>
2025-04-27 13:46:05 -0300

    add gluestack ui

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import "./global.css";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import { Login } from "./src/modules/auth/screens/Login";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open uedddd!</Text>
-      <StatusBar style="auto" />
-      <Text className="text-3xl text-red-900">Heqwello world!</Text>
-      <View className="bg-blue-500 w-12 h-10"></View>
-    </View>
+    <GluestackUIProvider mode="dark">
+      <View style={styles.container}>
+        <Login />
+      </View>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
   },
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  "react-dom": path.resolve(__dirname, "node_modules/react-native"),
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
