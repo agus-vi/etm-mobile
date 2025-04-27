30bc352f5b68f22acaf4129d6c08198177fc8389
agus-vi <agux_vi@hotmail.com>
2025-04-27 08:55:55 -0300

    config nativewind

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open uedddd!</Text>
       <StatusBar style="auto" />
+      <Text className="text-3xl text-red-900">Heqwello world!</Text>
+      <View className="bg-blue-500 w-12 h-10"></View>
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
